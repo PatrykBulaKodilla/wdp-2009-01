ee6f7e061d4d443059fef850e80c32742d0221f8
androszr <androszr@gmail.com>
2020-09-11 17:32:14 +0200

    Add event listener for current window width. Add set state for current responsive mode between desktop, tablet and mobile. Add function to get current responsive mode from state

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -4,16 +4,40 @@ import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const MainLayout = ({ children, setResponsiveMode, currentRwdMode }) => {
+  const checkRwdMode = windowInnerWidth => {
+    if (windowInnerWidth <= 576) {
+      //console.log('mobile');
+      setResponsiveMode('mobile');
+    } else if (windowInnerWidth > 576 && windowInnerWidth <= 768) {
+      //console.log('tablet');
+      setResponsiveMode('tablet');
+    } else {
+      //console.log('desktop');
+      setResponsiveMode('desktop');
+      //console.log('current mode is: ', currentRwdMode);
+    }
+  };
+
+  checkRwdMode(window.innerWidth);
+
+  window.addEventListener('resize', () => {
+    checkRwdMode(window.innerWidth);
+  });
+
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
+  setResponsiveMode: PropTypes.func,
+  currentRwdMode: PropTypes.string,
 };
 
 export default MainLayout;
