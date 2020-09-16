2ed631f8a5d6cc60f55fe28a361c1e9ba5655761
Paweł Matysiak <mat.pawel@gmail.com>
2020-09-16 20:17:32 +0200

    Add RWD on Brands (WDP200901-32)

--- src/components/features/Brands/BrandsContainer.js
+++ src/components/features/Brands/BrandsContainer.js
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux';
 import Brands from './Brands';
 
+import { currentRwdMode } from '../../../redux/responsiveRedux.js';
+
 const mapStateToProps = state => ({
   brands: state.brands,
+  currentRwdMode: currentRwdMode(state),
 });
 
 export default connect(mapStateToProps)(Brands);
